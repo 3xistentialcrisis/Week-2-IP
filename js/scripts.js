165e46c23eba32e52757c753802288fe1d7249f1
Wanjiku Karanja <njahirakaranja@Njahiras-MacBook-Air.local>
2019-09-22 23:57:04 +0300

    Edit birthday and gender forms

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,27 @@
-var bday = akan Date();
-var femaleakan = new Array(7);
-weekday[0] = "Akosua";
-weekday[1] = "Adwoa";
-weekday[2] = "Abenaa";
-weekday[3] = "Akua";
-weekday[4] = "Yaa";
-weekday[5] = "Afua";
-weekday[6] = "Ama";
+function getFemaleAkanName() {
+    var bday = new Date();
+    var femalebday = new Array(7);
+    weekday[0] = "Akosua";
+    weekday[1] = "Adwoa";
+    weekday[2] = "Abenaa";
+    weekday[3] = "Akua";
+    weekday[4] = "Yaa";
+    weekday[5] = "Afua";
+    weekday[6] = "Ama";
+
+    var femaleakan = femalebday[bday.getDay()]
+    alert(femaleakan);
+}
+function getMaleAkanName (){
+    var malebday = new Array(7);
+    weekday[0] = "Kwasi";
+    weekday[1] = "Kwadwo";
+    weekday[2] = "Kwabena";
+    weekday[3] = "Kwaku";
+    weekday[4] = "Yaw";
+    weekday[5] = "Kofi";
+    weekday[6] = "Kwame";
+
+    var maleakan = malebday[bday.getDay()]
+    alert(maleakan)
+}
