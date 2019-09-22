0342d07a8ab73b5327f5289745401b2d7e12791a
Wanjiku Karanja <njahirakaranja@Njahiras-MacBook-Air.local>
2019-09-23 02:37:54 +0300

    Edit birthday_form with birthday,male,female and result id's

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,20 @@
-var birthday=document.forms["birthday_form"].onsubmit
-alert(birthday)
+var birthday=document.getElementById("birthday")
+var male=document.getElementById("male")
+var female=document.getElementById("female")
+var result=document.getElementById("result")
+
+function a(){
+    var dates=new Date(birthday.value)
+    var day=dates.getDay();
+}
+    switch(day)
+    {
+        case 0:result.innerHTML="Akosua"; break;
+        case 1:result.innerHTML="Adwoa"; break;
+        case 2:result.innerHTML="Abenaa"; break;
+        case 3:result.innerHTML="Akua"; break;
+        case 4:result.innerHTML="Yaa"; break;
+        case 5:result.innerHTML="Afua"; break;
+        case 6:result.innerHTML="Ama"; break;
+    }
+    alert(day)
